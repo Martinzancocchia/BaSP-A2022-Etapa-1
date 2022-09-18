ea348a9987d14afc0be5bd8e709267f58256a1cd
martinzancocchia <tinchozan@gmail.com>
2022-09-18 15:06:01 -0300

    BaSP-W05-2022: Finished Ex 1 (variables and operators)

---
+++ Semana-05/Exercises/01_variables_and_operators.js
@@ -0,0 +1,23 @@
+console.log("Variables and operators:")
+
+// 1-a)
+
+var n1 = 10;
+var n2 = 5;
+var sum = n1 + n2;
+console.log("1-a) The result of " + n1 + " + " + n2 + " is: " + sum);
+
+// 1-b)
+
+var text1 = "Si te gusta el durazno... ";
+var text2 = "Bancate la pelusa. ";
+var wholeText = text1 + text2;
+console.log("1-b) " + wholeText);
+
+// 1-c)
+
+var text1_1 = "Si te gusta la milanesa... ";
+var text2_2 = "Bancate el rebozado. ";
+var textLength = text1_1.length + text2_2.length;
+var wholeText2 = text1_1 + text2_2;
+console.log(`1-c) The length of - ${wholeText2}- is: ${textLength} characters`);
